b82ee84c2d1bf0ec9e7ebc5c1485f5f913718998
Hxxseung <soaorsith2@naver.com>
2024-09-06 14:43:28 +0900

    coding test

---
+++ src/Lv0/printString.java
@@ -0,0 +1,11 @@
+package Lv0;
+
+import java.util.Scanner;
+
+public class printString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        System.out.println(a);
+    }
+}
