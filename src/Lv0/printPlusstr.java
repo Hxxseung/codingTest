61ff5b295468b7e5eaf6df8b108ced040d221ddc
Hxxseung <soaorsith2@naver.com>
2024-09-09 18:50:18 +0900

    coding test

---
+++ src/Lv0/printPlusstr.java
@@ -0,0 +1,13 @@
+package Lv0;
+
+import java.util.Scanner;
+
+public class printPlusstr {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+        String c = a+b;
+        System.out.println(c);
+    }
+}
