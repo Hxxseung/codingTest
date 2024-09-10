a96bdfc2394b73583b5b538e069f99bac4724710
Hxxseung <soaorsith2@naver.com>
2024-09-10 21:45:29 +0900

    coding test

---
+++ src/PCEE/PCEE_numDiv.java
@@ -0,0 +1,18 @@
+package PCEE;
+
+import java.util.Scanner;
+
+public class PCEE_numDiv {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        int answer = 0;
+
+        for(int i=0; number > 0; i++){
+            answer += number % 100;
+            number /= 100;
+        }
+
+        System.out.println(answer);
+    }
+}
