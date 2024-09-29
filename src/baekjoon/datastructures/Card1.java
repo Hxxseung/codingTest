5fcad2d687945f31a0411658be6e25b0b407b6a4
Hxxseung <soaorsith2@naver.com>
2024-09-29 10:10:01 +0900

    EscapeSquare problem & Card1(not solved)

---
+++ src/baekjoon/datastructures/Card1.java
@@ -0,0 +1,13 @@
+package baekjoon.datastructures;
+
+import java.util.Scanner;
+
+public class Card1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+
+
+        //푸는중
+    }
+}
