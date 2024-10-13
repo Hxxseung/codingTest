3ac95ecc5d240f539a383a087a773fe62310b8f6
Hxxseung <soaorsith2@naver.com>
2024-10-13 22:28:58 +0900

    SortInside BeakJoon 1427

--- src/baekjoon/sorting/SortInside.java
+++ src/baekjoon/sorting/SortInside.java
@@ -1,11 +1,19 @@
 package baekjoon.sorting;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class SortInside {
     public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
 
+        char[] sort = input.toCharArray();
+        Arrays.sort(sort);
+
+        sb.append(sort);
+        sb.reverse();
+        System.out.println(sb);
     }
-
 }
-
-//오름차순 정렬 후 reverse쓰면 될 듯
-//푸는중
