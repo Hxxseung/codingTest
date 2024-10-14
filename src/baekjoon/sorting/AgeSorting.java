eb9efd4c94a93e558157e3cfc30ad29c9d8b61e7
Hxxseung <soaorsith2@naver.com>
2024-10-14 17:31:20 +0900

    AgeSorting BeakJoon 10814

--- src/baekjoon/sorting/AgeSorting.java
+++ src/baekjoon/sorting/AgeSorting.java
@@ -1,7 +1,32 @@
 package baekjoon.sorting;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
 public class AgeSorting {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String[][] arr = new String[n][2];
+
+        for (int i = 0; i < n; i++) {
+            arr[i][0] = sc.next();
+            arr[i][1] = sc.next();
+        }
+
+        Arrays.sort(arr, new Comparator<String[]>() {
+            @Override
+            public int compare(String[] s1, String[] s2) {
+                return Integer.parseInt(s1[0]) - Integer.parseInt(s2[0]);
+            }
+        });
+
+        for(int i = 0; i < n; i++) {
+            System.out.println(arr[i][0] + " " + arr[i][1]);
+        }
 
     }
 }
+
+// Beakjoon 10814 : memory : 105860 KB / Time : 1552 ms
