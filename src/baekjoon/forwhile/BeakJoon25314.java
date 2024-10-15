7fc753468650b75fe17764664762834a48819931
Hxxseung <soaorsith2@naver.com>
2024-10-16 01:42:24 +0900

    BeakJoon25314

---
+++ src/baekjoon/forwhile/BeakJoon25314.java
@@ -0,0 +1,17 @@
+package baekjoon.forwhile;
+
+import java.util.Scanner;
+
+public class BeakJoon25314 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+            if(n % 4 == 0) {
+                for (int i = 0; i < n/4; i++) {
+                    System.out.print("long ");
+                }
+            }
+        System.out.println("int");
+    }
+}
