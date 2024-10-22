771bff82cedc80d409d8377a860e44d7bff818a9
Hxxseung <soaorsith2@naver.com>
2024-10-22 20:16:53 +0900

    BeakJoon11729

--- src/baekjoon/recursion/BeakJoon11729.java
+++ src/baekjoon/recursion/BeakJoon11729.java
@@ -1,4 +1,33 @@
 package baekjoon.recursion;
 
+import java.util.Scanner;
+
 public class BeakJoon11729 {
+
+    public static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        sb.append( (int) (Math.pow(2 , N) - 1 )).append('\n');
+
+        Hanoi(N, 1, 2, 3);
+        System.out.println(sb);
+    }
+
+    private static void Hanoi(int N, int Start, int Mid, int End) {
+
+        if (N == 1){
+            sb.append(Start + " " + End + "\n");
+            return;
+        }
+
+        Hanoi(N-1, Start, End, Mid);
+
+        sb.append(Start + " " + End + "\n");
+
+        Hanoi(N - 1, Mid, Start, End);
+    }
 }
