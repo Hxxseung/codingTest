d56180f6c6c3e7d4280fcf197456a4f495da9125
Hxxseung <soaorsith2@naver.com>
2024-11-14 10:21:26 +0900

    두 정수 사이 합

---
+++ src/programmers/lv1/IntegerSum.java
@@ -0,0 +1,18 @@
+package programmers.lv1;
+
+public class IntegerSum {
+    class Solution {
+        public long solution(int a, int b) {
+            long answer = 0;
+            if (a > b) {
+                int temp = a;
+                a = b;
+                b = temp;
+            }
+            for (int i = a; i <= b; i++) {
+                answer += i;
+            }
+            return answer;
+        }
+    }
+}
