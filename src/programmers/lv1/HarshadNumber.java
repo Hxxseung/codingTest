96c17cfd5c45f8416e135ae23743922880b8f9cc
Hxxseung <soaorsith2@naver.com>
2024-11-14 10:11:44 +0900

    하샤드 수

---
+++ src/programmers/lv1/HarshadNumber.java
@@ -0,0 +1,15 @@
+package programmers.lv1;
+
+public class HarshadNumber {
+    class Solution {
+        public boolean solution(int x) {
+            int sum = 0;
+            int original = x;
+            while(x>0){
+                sum+=x%10;
+                x/=10;
+            }
+            return original % sum == 0;
+        }
+    }
+}
