6fbce5187d784914a92983ab9e2aa57349b34382
Hxxseung <soaorsith2@naver.com>
2024-11-19 10:38:02 +0900

    서울에서 김서방 찾기

---
+++ src/programmers/lv1/FindKim.java
@@ -0,0 +1,16 @@
+package programmers.lv1;
+
+public class FindKim {
+    class Solution {
+        public String solution(String[] seoul) {
+            String answer = "";
+
+            for(int i=0; i<seoul.length; i++) {
+                if(seoul[i].equals("Kim")) {
+                    answer = "김서방은 " + i + "에 있다";
+                }
+            }
+            return answer;
+        }
+    }
+}
