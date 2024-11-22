3747f5fee830eecaa94d2f3044d57f154f212506
Hxxseung <soaorsith2@naver.com>
2024-11-22 10:09:04 +0900

    BeakJoon 10811

---
+++ src/baekjoon/array/BeakJoon10811.java
@@ -0,0 +1,40 @@
+package baekjoon.array;
+
+import java.util.Scanner;
+
+public class BeakJoon10811 {
+        public static void main(String[] args){
+            Scanner sc = new Scanner(System.in);
+
+            int n = sc.nextInt();
+            int count = sc.nextInt();
+
+            int temp=0;
+
+            int busket[] = new int[n];
+            for(int i=0;i<busket.length;i++) {
+                busket[i] = i+1;
+            }
+
+            for(int i=0;i<count;i++) {
+                int a = sc.nextInt()-1;
+                int b = sc.nextInt()-1;
+
+                while(a<b) {
+                    temp = busket[a];
+                    busket[a] = busket[b];
+                    busket[b] = temp;
+                    a++;
+                    b--;
+                }
+            }
+            sc.close();
+
+            for(int i=0;i<busket.length;i++) {
+                System.out.print(busket[i]+" ");
+            }
+
+
+        }
+    }
+
